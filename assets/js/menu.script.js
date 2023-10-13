b8a1e088b89a3d27c66b8789343c32f8cb46279b
Leandro Ferrero <learferrero@gmail.com>
2023-10-12 21:46:40 -0300

    Codigo mejor organizado y menu actualizado

--- assets/js/menu.script.js
+++ assets/js/menu.script.js
@@ -1,7 +1,15 @@
 const hamMenu = document.querySelector(".hamburger-menu");
 const offScreenMenu = document.querySelector(".off-screen-menu");
+const linksList = document.querySelectorAll("li a");
 
 hamMenu.addEventListener("click", () => {
     hamMenu.classList.toggle("active");
     offScreenMenu.classList.toggle("active");
+});
+
+linksList.forEach(link => {
+    link.addEventListener("click", () => {
+        hamMenu.classList.toggle("active");
+        offScreenMenu.classList.toggle("active");
+    });
 });
