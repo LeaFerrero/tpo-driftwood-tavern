d85b35695d0f497ab32568f23b48111285a0e55b
Leandro Ferrero <learferrero@gmail.com>
2023-10-13 22:20:34 -0300

    Agregado footer con estilo

--- assets/js/menu.script.js
+++ assets/js/menu.script.js
@@ -1,6 +1,6 @@
 const hamMenu = document.querySelector(".hamburger-menu");
 const offScreenMenu = document.querySelector(".off-screen-menu");
-const linksList = document.querySelectorAll("li a");
+const linksList = document.querySelectorAll(".navigation-links-list li");
 
 hamMenu.addEventListener("click", () => {
     hamMenu.classList.toggle("active");
@@ -11,5 +11,6 @@ linksList.forEach(link => {
     link.addEventListener("click", () => {
         hamMenu.classList.toggle("active");
         offScreenMenu.classList.toggle("active");
+        
     });
 });
