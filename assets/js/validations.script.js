a34f5ae36f64edbef406d8393e1ee7e7b2508821
Leandro Ferrero <learferrero@gmail.com>
2023-10-19 16:09:09 -0300

    Traducido a ingles para mas cosistencia con el consumo de la api y errores coregidos

--- assets/js/validations.script.js
+++ assets/js/validations.script.js
@@ -1,3 +1,5 @@
+const contactForm = document.getElementById("contact-form");
+
 // Función para validar el campo de texto (al menos 1 palabra y menos de 25)
 const validateTextArea = (input) => {
   const words = input.split(/\s+/).filter((word) => word.length > 0);
@@ -5,9 +7,9 @@ const validateTextArea = (input) => {
 };
 
 // Función para validar el formato de correo electrónico
-const validateEmail = (mail) => {
+const validateEmail = (email) => {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return emailRegex.test(mail);
+  return emailRegex.test(email);
 };
 
 // Función para validar el formulario
@@ -16,35 +18,37 @@ const validateForm = (formData) => {
   let isValid = true;
   
   const name = formData.get("name");
-  const mail = formData.get("mail");
+  const email = formData.get("email");
   const message = formData.get("message");
 
-  const nameInput = document.getElementById("name");
-  const mailInput = document.getElementById("mail");
-  const messageInput = document.getElementById("message");
-
+  const nameInput = document.getElementById("error-name");
+  const mailInput = document.getElementById("error-email");
+  const messageInput = document.getElementById("error-message");
+  
+  //Limpia los mensajes de error 
+  nameInput.textContent = "";
+  mailInput.textContent = ""; 
+  messageInput.textContent = "";
+  
   //Retorna falso en caso de que no se cumpla la validacion
   //Imprime el mensaje de error en el campo que corresponda
   if (name.trim().length < 3) 
   {
-    nameInput.value = "";
-    nameInput.placeholder = "El nombre debe tener al menos 3 letras.";
+    nameInput.textContent = "Name needs 3+ letters."
   
     isValid = false;
   }
 
-  if (!validateEmail(mail)) 
+  if (!validateEmail(email)) 
   {
-    mailInput.value = ""; 
-    mailInput.placeholder = "Ingrese un email válido.";
+    mailInput.textContent = "Enter a valid email."
 
     isValid = false;
   }
 
   if (!validateTextArea(message)) 
   {
-    messageInput.value = "";
-    messageInput.placeholder = "El área de texto debe contener más hasta 25 palabras.";
+    messageInput.textContent = "Message limited to 25 words."
 
     isValid = false;
   }
@@ -56,11 +60,16 @@ const validateForm = (formData) => {
 document.getElementById("contact-form").addEventListener("submit", (event) => {
   const formData = new FormData(event.target);
 
-  if (!validateForm(formData)) {
+  if (!validateForm(formData)) 
+  {
         event.preventDefault(); // Evitar que el formulario se envíe si la validación falla
   }
   else
   {
+     // Limpiar campos del formulario
+     document.getElementById("name").value = "";
+     document.getElementById("email").value = "";
+     document.getElementById("message").value = "";
     alert("Formulario enviado, encuanto podamos nos pondremos en contacto.");
   }
 });
